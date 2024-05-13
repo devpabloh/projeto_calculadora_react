11a07e36435cd1c761ea94ece3c7455a416813ad
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-13 15:39:23 -0300

    atualização

--- projeto-react-calculadora/src/style.js
+++ projeto-react-calculadora/src/style.js
@@ -1,6 +1,10 @@
-import {} from 'styled-components';
+import styled from 'styled-components';
 
-export const Container = styled.div
+export const Container = styled.div`
 width:100%;
 height:100vh;
-background-color: #FAFAFA
+background-color: #FAFAFA; 
+`
+
+
+
